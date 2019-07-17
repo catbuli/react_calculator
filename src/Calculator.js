43fb6dcb2b02426f5f70c5d7d19869eddf43f1b8
zhangyifei233 <15204944127@163.com>
2019-07-17 15:00:18 +0800

    新增lib文件

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Input, Button } from 'antd';
+import { stringToArray, infixToSuffix,suffixResult } from "./lib"
 import { inputAction, claerAction, runAction, backSpaceAction } from './store/actionCreators'
 import store from './store'
 import 'antd/dist/antd.css'
@@ -38,7 +39,7 @@ class Calculator extends Component {
         switch (count) {
             case "C": action = claerAction(); break;
             case "=": {
-                let result = this.suffixResult(this.suffix(this.stringToArray(this.state.equation)))
+                let result = suffixResult(infixToSuffix(stringToArray(this.state.equation)))
                 action = runAction(result);
                 break;
             }
@@ -57,111 +58,6 @@ class Calculator extends Component {
     backSpace(str) {
         str = str.substring(0, str.length - 1);
         return str;
-    }
-    //字符串转数组
-    stringToArray(str) {
-        let data = str.split('')
-        var flag = ""
-        let result = [];
-        data.forEach((data, index) => {
-            if (data !== "+" && data !== "-" && data !== "*" && data !== "/") {
-                flag = flag + data;
-            } else {
-                result.push(parseFloat(flag))
-                result.push(data)
-                flag = ""
-            }
-
-        })
-        result.push(parseFloat(flag))
-        return result
-    }
-    //  中缀表达式转后缀表达式
-    suffix(data) {
-        let str = ""
-        let operator = []
-        data.forEach((a, index) => {
-            if (Number(a)) {
-                str = str + a + " ";
-            } else {
-                if (operator.length === 0) {
-                    operator.push(a)
-                } else {
-                    let test = operator.length
-                    for (let index = 0; index <= test; index++) {
-                        let tmp = operator.pop()
-                        operator.push(tmp)
-                        let flag = this.operatorCompare(a, tmp)
-                        if (flag && operator.length !== 0) {
-                            str = str + operator.pop() + " ";
-                        } else {
-                            operator.push(a)
-                            break;
-                        }
-                    }
-                }
-            }
-        })
-        while (operator.length !== 0) {
-            str = str + operator.pop() + " ";
-        }
-        console.log(str);
-        return str;
-    }
-    //后缀表达式运算
-    suffixResult(str) {
-        let data = str.split(' ');
-        let result = []
-        data.forEach((data) => {
-            if (!isNaN(parseFloat(data))) {
-                result.push(parseFloat(data))
-            } else {
-                switch (data) {
-                    case "+":
-                        result.push(parseFloat(result.pop()) + parseFloat(result.pop()))
-                        break;
-                    case "-":
-                        let a = parseFloat(result.pop()); let b = parseFloat(result.pop())
-                        result.push(b - a);
-                        break;
-                    case "*":
-                        result.push(parseFloat(result.pop()) * parseFloat(result.pop()))
-                        break;
-                    case "/":
-                        let aa = parseFloat(result.pop()); let bb = parseFloat(result.pop())
-                        result.push(bb / aa);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        })
-        return result.pop()
-    }
-    //运算符比较优先级
-    operatorCompare(a, b) {
-        var add = "+", minu = "-", multiply = "*", divide = "/"
-        var flaga;
-        var flagb;
-        switch (a) {
-            case add: flaga = 1; break;
-            case minu: flaga = 1; break;
-            case multiply: flaga = 2; break;
-            case divide: flaga = 2; break;
-            default:
-                break;
-        }
-        switch (b) {
-            case add: flagb = 1; break;
-            case minu: flagb = 1; break;
-            case multiply: flagb = 2; break;
-            case divide: flagb = 2; break;
-            default:
-                break;
-        }
-        console.log(flaga, flagb);
-
-        return flaga <= flagb
     }
     render() {
         var buttonList = [];
---
+++ src/lib.js
@@ -0,0 +1,135 @@
+/**
+ *  将公式字符串转换为数组 将公式中的数组转换为Number 
+ *
+ * @param {string} str - 传入的公式字符串
+ * @returns {Array} - 返回的数组 
+ */
+function stringToArray(str) {
+    if (str !== "" && str !== undefined) {
+        let data = str.split('')
+        var flag = ""
+        let arr = [];
+        data.forEach((data) => {
+            if (data !== "+" && data !== "-" && data !== "*" && data !== "/") {
+                flag = flag + data;
+            } else {
+                arr.push(parseFloat(flag))
+                arr.push(data)
+                flag = ""
+            }
+        })
+        arr.push(parseFloat(flag))
+        return arr
+    }
+}
+
+/**
+ * 将stringToArray方法中生成的数组 转换为后缀表达式
+ *
+ * @param {arr} data
+ * @returns
+ */
+function infixToSuffix(arr) {
+    if (arr !== undefined) {
+        let str = ""
+        let operator = []
+        arr.forEach((a) => {
+            if (Number(a)) {
+                str = str + a + " ";
+            } else {
+                if (operator.length === 0) {
+                    operator.push(a)
+                } else {
+                    let test = operator.length
+                    for (let index = 0; index <= test; index++) {
+                        let tmp = operator.pop()
+                        operator.push(tmp)
+                        let flag = operatorCompare(a, tmp)
+                        if (flag && operator.length !== 0) {
+                            str = str + operator.pop() + " ";
+                        } else {
+                            operator.push(a)
+                            break;
+                        }
+                    }
+                }
+            }
+        })
+        while (operator.length !== 0) {
+            str = str + operator.pop() + " ";
+        }
+
+        return str;
+    }
+}
+
+
+/**
+ *
+ *
+ * @param {*} str
+ * @returns
+ */
+function suffixResult(str) {
+    if (str !== undefined) {
+        let data = str.split(' ');
+        let result = []
+        data.forEach((data) => {
+            if (!isNaN(parseFloat(data))) {
+                result.push(parseFloat(data))
+            } else {
+                switch (data) {
+                    case "+":
+                        result.push(parseFloat(result.pop()) + parseFloat(result.pop()))
+                        break;
+                    case "-":
+                        let a = parseFloat(result.pop()); let b = parseFloat(result.pop())
+                        result.push(b - a);
+                        break;
+                    case "*":
+                        result.push(parseFloat(result.pop()) * parseFloat(result.pop()))
+                        break;
+                    case "/":
+                        let aa = parseFloat(result.pop()); let bb = parseFloat(result.pop())
+                        result.push(bb / aa);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        })
+        return result.pop()
+    }
+}
+/**
+ *
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns
+ */
+function operatorCompare(a, b) {
+    var add = "+", minu = "-", multiply = "*", divide = "/"
+    var flaga;
+    var flagb;
+    switch (a) {
+        case add: flaga = 1; break;
+        case minu: flaga = 1; break;
+        case multiply: flaga = 2; break;
+        case divide: flaga = 2; break;
+        default:
+            break;
+    }
+    switch (b) {
+        case add: flagb = 1; break;
+        case minu: flagb = 1; break;
+        case multiply: flagb = 2; break;
+        case divide: flagb = 2; break;
+        default:
+            break;
+    }
+    return flaga <= flagb
+}
+
+
+export { stringToArray, infixToSuffix, suffixResult, operatorCompare }
